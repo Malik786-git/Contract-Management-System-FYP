549351f2f9156fb24d5404cce5056c8b890d67e6
ahmedashar <ahmedashar12345@gmail.com>
2024-02-04 06:50:43 +0500

    byashar

--- client/src/components/Dashbord.tsx
+++ client/src/components/Dashbord.tsx
@@ -1,41 +1,78 @@
 import React, { useState, useEffect } from "react";
 import FormData from "./Type";
 import { BiSearch } from "react-icons/bi";
-
+import instance from "../../axiosConfig";
+import { useAppSelector } from "../hooks/redux-hooks";
+import { Table } from "react-bootstrap";
+import moment from "moment";
 const Dashboard: React.FC = () => {
   const [records, setRecords] = useState<FormData[]>([]);
 
+  const {role, _id} = useAppSelector((state) => state.data.user)
+  console.log("role", role)
+
   useEffect(() => {
-    const storedRecords = JSON.parse(
-      localStorage.getItem("AddItems") || "[]"
-    ) as FormData[];
-    setRecords(storedRecords);
+    // const storedRecords = JSON.parse(
+    //   localStorage.getItem("AddItems") || "[]"
+    // ) as FormData[];
+    // setRecords(storedRecords);
+    getContracts();
   }, []);
+  const getContracts = async () => {
+    try {
+        const route = (role == "admin") ? "api/contract/all-contracts" : `/api/contract/user-contracts/${_id}`;
+        const response = await instance.get(route)
+        console.log("response", response.data.contracts)
+        setRecords(response?.data?.contracts)
+      } catch (error) {
 
-  return (
-    <div>
-      <div className="dashboard">
-        <h3>Contracts</h3>
-        <div className="input-search">
-          <div>
-            <input placeholder="Search" />
-          </div>
+}
+    }
+return (
+  <div className="dashboard_container ">
+    <div className="dashboard">
+      <h3>Contracts</h3>
+      <div className="input-search">
+        <div>
+          <input placeholder="Search" />
+        </div>
 
-          <div className="input-search-icon">
-            <BiSearch />
-          </div>
+        <div className="input-search-icon">
+          <BiSearch />
         </div>
       </div>
-      <ul className="dashboard-list">
-        {records.map((record, index) => (
-          <li key={index}>
-            ID: {record.id}, Duration: {record.duration}, Project Name:{" "}
-            {record.projectName}{" "}
-          </li>
-        ))}
-      </ul>
     </div>
-  );
+    <Table className="dashboard_table mt-3">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Duration</th>
+            <th>Project Name</th>
+            <th>Budget</th>
+            <th>Status</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Contractor</th>
+          </tr>
+        </thead>
+        <tbody>
+        {records.map((item,index)=>(
+          <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item.duration}</td>
+              <td>{item.project_name}</td>
+              <td>{item.budget}</td>
+              <td style={{color: (item.status == "incomplete")? "red":"green"}}>{item.status}</td>
+              <td>{moment(item.started_date).format('DD-MM-YYYY')}</td>
+              <td>{moment(item.end_date).format('DD-MM-YYYY')}</td>
+              <td>{item.user_id.name}</td>
+          </tr>
+        ))}  
+        </tbody>
+      </Table>
+      
+  </div>
+);
 };
 
 export default Dashboard;
